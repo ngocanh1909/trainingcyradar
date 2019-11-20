cba7e1df0bc41b3185a63d1cbee82bbc28fc4046
ngocanh1909 <anhnhubasy@gmail.com>
2019-11-20 17:16:36 +0700

    fix format code

--- task1.go
+++ task1.go
@@ -11,134 +11,125 @@ import (
 func request(url string) (string, error){
 	resp, err := http.Get(url)
 	if err != nil{
-		return "", err// neu get loi thi luu vet r exit
+		return "", err
 	}
-	defer resp.Body.Close() // tri hoan dong body
-	body, err := ioutil.ReadAll(resp.Body) // doc du lieu request body su dung
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil{
-		return "", err //neu doc du lieu loi thi...
+		return "", err
 
 	}
 	return string(body), err
 }
-//struct
-type malshare_data struct {
+type malshareData struct {
 	md5 []string
 	sha1 []string
 	sha256 []string
 }
-//ham lay md5, sha1, sha256 trong file
-func get_hash(url string) malshare_data{
-	var result malshare_data
-	data_str, err := request(url) //gan data_str = du lieu request body
+func getHash(url string) malshareData{
+	var result malshareData
+	dataStr, err := request(url)
 	if err != nil{
 		return result
 	}
-	var md5_array [] string
-	var sha1_array [] string
-	var sha256_array [] string
+	var md5Array [] string
+	var sha1Array [] string
+	var sha256Array [] string
 	for{
-		if (len(data_str)==0){ // doc du lieu request body = 0 thi dung
+		if (len(dataStr)==0){
 			break
 		}
-		md5_str := data_str[0:31] // do dai cua md5
-		sha1_str := data_str[33:73] // do dai...
-		sha256_str := data_str[74:138] // do dai...
-		i := strings.Index(data_str, "\n") // du lieu co 4 cot tren 1 hang, tim moi cot tren tung hang
-		data_str = data_str[i+1 : len(data_str) ] // cat tu vi tri i+1 den het do dai cua data_str
-		md5_array = append(md5_array, md5_str) // them tung phan tu md5 vao mang
-		sha1_array = append(sha1_array, sha1_str) // them...
-		sha256_array = append(sha256_array, sha256_str) //them...
+		md5Str := dataStr[0:31]
+		sha1Str := dataStr[33:73]
+		sha256Str := dataStr[74:138]
+		i := strings.Index(dataStr, "\n")
+		dataStr = dataStr[i+1 : len(dataStr) ]
+		md5Array = append(md5Array, md5Str)
+		sha1Array = append(sha1Array, sha1Str)
+		sha256Array = append(sha256Array, sha256Str)
 
 	}
-	result.md5 = md5_array //tra ve map mang md5
-	result.sha1 = sha1_array
-	result.sha256 = sha256_array
+	result.md5 = md5Array
+	result.sha1 = sha1Array
+	result.sha256 = sha256Array
 	return result
 }
 
-func dump_data() map[string] malshare_data {
-	malshare_map := make(map[string] malshare_data)
-	body_str, err := request("https://malshare.com/daily/") // gan body_str = du lieu request body url
-	//host_str := "https://malshare.com/daily/"
+func dumpData() map[string] malshareData {
+	malshareMap := make(map[string] malshareData)
+	bodyStr, err := request("https://malshare.com/daily/")
 	if err != nil{
-		return malshare_map
+		return malshareMap
 	}
 	magic := regexp.MustCompile(`\"\[DIR\]\"></[a-z]{2}><[a-z]{2}><a\s[a-z]{4}=\"`)
-	magic_str := string(magic.Find([]byte(body_str)))
+	magicStr := string(magic.Find([]byte(bodyStr)))
 	end := regexp.MustCompile("_[a-z]{8}/")
-	out_end := string(end.Find([]byte(body_str)))
-	//dem := 0
+	outEnd := string(end.Find([]byte(bodyStr)))
+	dem := 0
 	for {
-		i := strings.Index(body_str, magic_str) // tim kiem doan magic trong chuoi string body url
+		i := strings.Index(bodyStr, magicStr)
 		re := regexp.MustCompile("=\"[0-9]{4}-\\d{2}-\\d{2}")
-		out := re.Find([]byte (body_str))
-		date_str := string(out)[2:]
-		body_str = body_str[i+len(magic_str)+1: len(body_str)]
-		if date_str == out_end{
+		out := re.Find([]byte (bodyStr))
+		dateStr := string(out)[2:]
+		bodyStr = bodyStr[i+len(magicStr)+1: len(bodyStr)]
+		if dateStr == outEnd{
 			break
 		}
-		url_str := fmt.Sprintf("https://malshare.com/daily/%s/malshare_fileList.%s.all.txt", date_str, date_str)
+		url_str := fmt.Sprintf("https://malshare.com/daily/%s/malshare_fileList.%s.all.txt", dateStr, dateStr)
 		fmt.Println(url_str)
-		hash_map := get_hash(url_str) // gan hash_map bang cac doan hash ham get_hash...
-		malshare_map[date_str] = hash_map // luu cac doan hash trong mang hash_map vao mang malshare_map ngay thang...
-		//dem=dem+1
-		//if(dem>100){
-		//	break
-		//}
+		hash_map := getHash(url_str)
+		malshareMap[dateStr] = hash_map
+		dem=dem+1
+		if(dem>100){
+			break
+		}
 	}
-	return malshare_map
+	return malshareMap
 }
-func orginize_data(malshare_map map[string] malshare_data){
-	for k := range malshare_map { // dung for duyet mang malshare_map de lay cac gia tri date_str
-		//fmt.Println(k)
-		yyyy := k[0:4] //cat gia tri yyyy
-		mm := k[5:7] //...
-		dd := k[8:10] //...
+func orginizeData(malshareMap map[string] malshareData){
+	for k := range malshareMap {
+		yyyy := k[0:4]
+		mm := k[5:7]
+		dd := k[8:10]
 		yyyy_path:=fmt.Sprintf("./%s",yyyy)
 		mm_path:=fmt.Sprintf("%s/%s",yyyy_path,mm)
 		dd_path:=fmt.Sprintf("%s/%s",mm_path,dd)
-		//tao folder yyyy, neu chua ton tai
 		if _, err := os.Stat(yyyy_path); os.IsNotExist(err) {
 			os.Mkdir(yyyy_path,777)
 		}
-		//tao folder mm trong yyyy
 		if _, err := os.Stat(mm_path); os.IsNotExist(err) {
 			os.Mkdir(mm_path,777)
 		}
-		//tao folder dd trong yyyy/mm/
 		if _, err := os.Stat(dd_path); os.IsNotExist(err) {
 			os.Mkdir(dd_path,777)
 		}
-		//ghi file md5.txt trong folder yyyy/mm/dd
 		file, err := os.Create(fmt.Sprintf("%s/md5.txt", dd_path))
 		if err != nil{
-			continue //neu co loi thi ghi sang ngay tiep theo
+			continue
 		}
-		//duyet tung phan tu md5 trong mang malshare_map de ghi vao file
-		for i:=0; i<len(malshare_map[k].md5); i++{ // lay tu 0 den < tong so luong phan tu trong mang
-			file.WriteString(malshare_map[k].md5[i]+"\n")
+		for i:=0; i<len(malshareMap[k].md5); i++{
+			file.WriteString(malshareMap[k].md5[i]+"\n")
 		}
-		file.Close() //dong file sau khi ghi
+		file.Close()
 		file, err = os.Create(fmt.Sprintf("%s/sha1.txt", dd_path))
 		if err != nil{
 			continue
 		}
-		for i:=0; i<len(malshare_map[k].sha1); i++{
-			file.WriteString(malshare_map[k].sha1[i]+"\n")
+		for i:=0; i<len(malshareMap[k].sha1); i++{
+			file.WriteString(malshareMap[k].sha1[i]+"\n")
 		}
 		file.Close()
 		file, err = os.Create(fmt.Sprintf("%s/sha256.txt", dd_path))
 		if err != nil{
 			continue
 		}
-		for i:=0; i<len(malshare_map[k].sha256); i++{
-			file.WriteString(malshare_map[k].sha256[i]+"\n")
+		for i:=0; i<len(malshareMap[k].sha256); i++{
+			file.WriteString(malshareMap[k].sha256[i]+"\n")
 		}
 		file.Close()
 	}
 }
 func main()  {
-	var data = dump_data();
-	orginize_data(data)
+	var data = dumpData()
+	orginizeData(data)
 }
