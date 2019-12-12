728903ec77b26ddd8ab1535f3cf0b3c1a2b43281
ngocanh1909 <anhnhubasy@gmail.com>
2019-12-12 14:55:32 +0700

    return err

--- task3.go
+++ task3.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"errors"
 )
 type IPRange struct {
 	SyncToken    string `json:"syncToken"`
@@ -39,40 +40,49 @@ func offBit(val uint32, k_th int) uint32 {
 	result = val & operator
 	return result
 }
-func ipv4ToBit(ip string) uint32 {
+func ipv4ToBit(ip string) (uint32, error) {
 	parts := strings.Split(ip, ".")
 	var val32 uint32
 	if len(parts) < 4 {
-		fmt.Printf("This is NOT a valid IPv4 address")
+		return -1, errors.New("This is NOT a valid IPv4 address")
 	} else if len(parts) == 4 {
 		for i := 0; i < len(parts); i++ {
 			if j, err := strconv.Atoi(parts[i]); err == nil {
 				if j < 0 || j > 255 {
-					fmt.Printf("This is NOT a valid IPv4 address")
+					return -1, errors.New("This is NOT a valid IPv4 address")
 				}
 				val32 = val32*256 + uint32(j)
 			}
 		}
 	}
-	return val32
+	return val32, nil
 }
-func handleIPRange(prefixes string) (uint32, uint32) {
+func handleIPRange(prefixes string) (uint32, uint32, error) {
 	iprangeIP := prefixes[0:strings.Index(prefixes, "/")]
-	val32 := ipv4ToBit(iprangeIP)
+	if len(iprangeIP) == 0{
+		return -1, -1, errors.New("This is NOT a valid IPv4 address")
+	}
+	val32, err := ipv4ToBit(iprangeIP)
+	if err != nil{
+		return -1, -1, errors.New("This is NOT a valid IPv4 address")
+	}
 	nb := prefixes[ strings.Index(prefixes, "/")+1:]
 	nbit, err := strconv.Atoi(nb)
 	if err != nil {
-		fmt.Printf("%s", err)
+		return -1, -1, err
 	}
 	var numberAnd uint32
 	numberAnd = ^numberAnd
 	for  i := 0; i<32-nbit; i++{
 		numberAnd = offBit(numberAnd,i)
 	}
-	return val32, numberAnd
+	return val32, numberAnd, nil
 }
 func handleIP(ip string, ipRange uint32, numberAnd uint32) uint32 {
-	val32 := ipv4ToBit(ip)
+	val32, err := ipv4ToBit(ip)
+	if err != nil{
+		return -1
+	}
 	val32 = val32 & numberAnd
 	return val32 ^ ipRange
 }
@@ -81,7 +91,7 @@ func main() {
 	defer timeTrack(time.Now(), fmt.Sprintf("Compare IP"))
 	read, err := ioutil.ReadFile("ip-ranges.json")
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	myJson := string(read)
 	json.Unmarshal([]byte(myJson), &parent)
@@ -95,9 +105,12 @@ func main() {
 	var iprangeArr [] uint32
 	var numberAndArr [] uint32
 	for i := 0; i < len(line); i++ {
-		iprange, so_de_and := handleIPRange(line[i])
+		iprange, numberAnd, err := handleIPRange(line[i])
 		iprangeArr = append(iprangeArr, iprange)
-		numberAndArr = append(numberAndArr, so_de_and)
+		numberAndArr = append(numberAndArr, numberAnd)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	fmt.Printf("%b ", numberAndArr)
 	file, err := os.Open("all.txt")
