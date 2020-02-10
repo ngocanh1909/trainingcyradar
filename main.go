52bb8b1170ddcd0fdd9c9672327fa7c1f99a8db0
ANH TRINH <anhnhubasy@gmail.com>
2020-02-10 10:00:05 +0700

    cli

--- main.go
+++ main.go
@@ -1,29 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/ngocanh1909/trainingcyradar/config"
 	"github.com/ngocanh1909/trainingcyradar/crawl"
 	"github.com/ngocanh1909/trainingcyradar/save"
+	"flag"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
+	"net/http"
 	"time"
 )
 
 const (
-	hosts    = "localhost:27017"
-	database = "crawl"
-	username = "admin1"
-	password = "admin1"
+	hosts      = "localhost:27017"
+	database   = "crawl"
+	username   = "admin1"
+	password   = "admin1"
 	collection = "post"
 )
 
-type Post struct {
-	Tile    string
-	Content string
+type MalshareDAO struct {
+	*mgo.Database
+}
+
+func (mal *MalshareDAO) processGET(c *gin.Context) {
+	malshareData := config.MalshareData{}
+	date := c.Params.ByName("date")
+	dateParse, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	}
+	query := bson.M{
+		"date": dateParse,
+	}
+	err = mal.C(collection).Find(query).One(&malshareData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"messages": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"messages": "ok",
+		"date":     dateParse,
+		"md5":      malshareData.Md5,
+		"sha1":     malshareData.Sha1,
+		"sha256":   malshareData.Sha256,
+	})
+}
+
+func (mal *MalshareDAO) setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/malshare/:date", mal.processGET)
+	return r
 }
 
 func main() {
-	// Tạo phiên kết nối với MongDB
 	info := &mgo.DialInfo{
 		Addrs:    []string{hosts},
 		Timeout:  60 * time.Second,
@@ -33,13 +70,24 @@ func main() {
 	}
 	session, err := mgo.DialWithInfo(info)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	var hashData []config.MalshareData
 	hashData, err = crawl.DumpData(&config.WaitGroup{})
-	fmt.Println("1")
-	save.SaveMgo(session.DB(database), hashData)
-	for i:=0; i<len (hashData); i++{
-		save.SaveFile(hashData[i])
+	wordPtr := flag.String("command", "crawl", "a string")
+	flag.Parse()
+	if (*wordPtr == "file") {
+		for i := 0; i < len(hashData); i++ {
+			save.SaveFile(hashData[i])
+		}
+	}
+	if (*wordPtr == "mgo") {
+		save.SaveMgo(session.DB(database), hashData)
+
+	}
+	if (*wordPtr == "api") {
+		d := MalshareDAO{session.DB(database)}
+		r := d.setupRouter()
+		r.Run(":8080")
 	}
 }
