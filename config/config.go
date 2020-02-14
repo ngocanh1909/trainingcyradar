d954bb7ba5dab5bda0cd041d0468296eaa92eb86
ANH TRINH <anhnhubasy@gmail.com>
2020-02-14 17:29:47 +0700

    config

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 type Config struct {
 	DB struct {
 		Server     string `toml:"server"`
+		Port       string `toml:"port"`
 		Database   string `toml:"database"`
 		Collection string `toml:"collection"`
 		Username   string `toml:"username"`
