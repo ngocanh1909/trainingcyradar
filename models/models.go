356b2987db8c5a0facb180a0d3829bc0e2389697
ANH TRINH <anhnhubasy@gmail.com>
2020-02-14 17:31:03 +0700

    err

--- models/models.go
+++ models/models.go
@@ -11,5 +11,6 @@ type MalshareData struct {
 	Md5    string        `json:"md5" bson:"md5"`
 	Sha1   string        `json:"sha1" bson:"sha1"`
 	Sha256 string        `json:"sha256" bson:"sha256"`
+	Err    error         `json:"err" bson:"err"`
 }
 
